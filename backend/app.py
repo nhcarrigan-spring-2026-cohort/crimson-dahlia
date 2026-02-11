8c7c146548fb5ee0eee555a0267205c04b9976c2
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2026-02-11 12:34:36 -0700

    Add models and extensions

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from extensions import  db, migrate
 from routes.users import users_bp
 from routes.tasks import tasks_bp
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+from models import User
 
 app.register_blueprint(users_bp, url_prefix="/users")
 app.register_blueprint(tasks_bp, url_prefix="/tasks")
