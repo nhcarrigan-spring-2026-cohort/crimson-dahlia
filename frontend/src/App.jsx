ddaa0602c4ff31902daf5140719893d7e086de5e
Harsh Vishwakarma <harsh.2428cseai2360@kiet.edu>
2026-02-24 19:01:22 +0530

    Add Profile Page Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import CreateAccount from "./components/CreateAccount";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
