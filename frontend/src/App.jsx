7ec2bf4319eb31d9944b487b64acb8e7c9151149
babyfacedkaiju <siyabulelashabangu@gmail.com>
2026-03-01 12:46:41 +0530

    implement My Applications page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import MyApplications from "./components/MyApplications";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import CreateAccount from "./components/CreateAccount";
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/my-applications" element={<MyApplications />} />
         <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/dashboard" element={<Dashboard />} />
